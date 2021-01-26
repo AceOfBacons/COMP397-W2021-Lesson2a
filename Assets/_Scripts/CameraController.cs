1da850d3bfa430a1216448180d72211468b3931c
Pablo Saldarriaga <psaldarr@my.centennialcollege.ca>
2021-01-25 21:37:58 -0500

    Camera controller

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float mouseSensitivity = 1000.0f;
+    public Transform playerBody;
+
+    private float Xrotation = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //make cursor invisible
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        Xrotation -= mouseY;
+        Xrotation = Mathf.Clamp(Xrotation, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.Euler(Xrotation, 0.00f, 0.0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,28 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementForce;
-    public Rigidbody rb;
+    public CharacterController controller;
+    public float maxSpeed = 10.0f;
+    public float gravity = -30.0f;
+    public float jumpHeight = 3.0f;
+
+    public Transform groundCheck;
+    public float groundRadius = 0.5f;
+    public LayerMask groundMask;
+    public Vector3 velocity;
+    
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0)
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundMask);
+
+        if(isGrounded && velocity.y < 0)
         {
-            //Move to the right
-            rb.AddForce(Vector3.right * movementForce);
+            velocity.y = -2.0f;
         }
 
-        if (Input.GetAxisRaw("Horizontal") < 0)
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * maxSpeed * Time.deltaTime);
+
+        if(Input.GetButton("Jump") && isGrounded)
         {
-            //Move to the left 
-            rb.AddForce(Vector3.left * movementForce);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
         }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+        }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
     }
+
 }
